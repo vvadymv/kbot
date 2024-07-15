46ed9d8023ba41d58c80701e7a5863cf4b86054d
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-07-15 19:56:35 +0300

    Add reply to hello - app version 1.0.2

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var appversion = "Version"
+var appVersion = "Version"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -22,7 +22,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(appversion)
+		fmt.Println(appVersion)
 	},
 }
 
